c5b973defe1b66b6395c51e63c1c203b17dc41b5
student <student@itstep1.dp.ua>
2025-01-21 15:16:40 +0200

    Initial commit

---
+++ HW1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Prgm
+{
+    public static void Main(string[] args)
+    {
+        int[] ar = new int[10]; Random rnd = new Random(); var txt = "I said that's a lie that I am racist";
+        for (int i = 0; i < ar.Length; i++) { ar[i] = rnd.Next(0, 100); }
+        Console.WriteLine($"{string.Join(",", ar)} | result: {txt} -> {string.Join("",Sub(txt.ToCharArray(), 26))}");
+        
+    }
+    static T[] Sub<T>(T[] ar, int offset = 0, int length = -1)
+    { // -1 means 'to the end'
+        length = length == -1 ? ar.Length - (offset) : length;
+        List<T> list = new List<T>();
+        for (int i = offset-1; i < length+offset; i++)
+        {
+            list.Add(ar[i]);
+        }
+        return list.ToArray();
+    }
+}
